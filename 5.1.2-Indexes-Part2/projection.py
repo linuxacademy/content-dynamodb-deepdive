730190c545d0fcaa9403d2388efb6d5527ebf254
Mark Richman <mark@markrichman.com>
2019-10-03 21:10:26 -0400

    Projection expression

---
+++ 5.1.2-Indexes-Part2/projection.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import decimal
+import json
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+dynamodb = boto3.resource("dynamodb")
+
+table = dynamodb.Table("album")
+
+# When making a Query call, you use the KeyConditionExpression parameter to
+# specify the hash key on which you want to query. If you want to use a
+# specific index, you also need to pass the IndexName.
+
+response = table.query(
+    IndexName="artist_id-index",
+    KeyConditionExpression=Key("artist_id").eq(192),
+    ProjectionExpression="title, price",
+)
+
+print("The query returned the following items:")
+for i in response[u"Items"]:
+    print(json.dumps(i, cls=DecimalEncoder))
