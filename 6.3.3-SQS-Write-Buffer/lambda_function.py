ea4c65d5a696eaf5b88db210069388b039e192a9
Mark Richman <mark@markrichman.com>
2019-10-22 20:56:10 -0400

    Lambda

---
+++ 6.3.3-SQS-Write-Buffer/lambda_function.py
@@ -0,0 +1,38 @@
+import decimal
+import json
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+QUEUE_NAME = os.environ["QUEUE_NAME"]
+MAX_QUEUE_MESSAGES = os.environ["MAX_QUEUE_MESSAGES"]
+DYNAMODB_TABLE = os.environ["DYNAMODB_TABLE"]
+
+sqs = boto3.resource("sqs")
+dynamodb = boto3.resource("dynamodb")
+
+
+def lambda_handler(event, context):
+
+    # Receive messages from SQS queue
+    queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
+
+    print(
+        "ApproximateNumberOfMessages:",
+        queue.attributes.get("ApproximateNumberOfMessages"),
+    )
+
+    for message in queue.receive_messages(MaxNumberOfMessages=int(MAX_QUEUE_MESSAGES)):
+        item = json.loads(message.body, parse_float=decimal.Decimal)
+        table = dynamodb.Table(DYNAMODB_TABLE)
+
+        try:
+            response = table.put_item(Item=item)
+            print("Wrote message to DynamoDB:", json.dumps(response))
+            message.delete()
+            print("Deleted message:", message.message_id)
+        except ClientError as e:
+            print(f'{e.response["Error"]["Code"]}: {e.response["Error"]["Message"]}')
+        else:
+            print(response)
