68ac634b71d6704c61fc8bd2401db078738bb014
Mark Richman <mark@markrichman.com>
2019-10-29 21:24:50 -0400

    Added ProjectionExpression

--- 6.4.1-FGAC/assume_role.py
+++ 6.4.1-FGAC/assume_role.py
@@ -4,6 +4,7 @@ import decimal
 import json
 
 import boto3
+from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
 
 
@@ -22,7 +23,8 @@ sts = boto3.client("sts")
 
 print("Default Provider Identity: : " + sts.get_caller_identity()["Arn"])
 
-role_to_assume_arn = "arn:aws:iam::408073669315:role/LimitedDynamoDBRole"
+# Replace with ARN of your limited DynamoDB role:
+role_to_assume_arn = "arn:aws:iam::123456789012:role/LimitedDynamoDBRole"
 role_session_name = "test_session"
 
 response = sts.assume_role(
@@ -40,9 +42,10 @@ sts_assumed_role = boto3.client(
 
 print("AssumedRole Identity: " + sts_assumed_role.get_caller_identity()["Arn"])
 
-# Create boto3 session with assumed role
+# Create boto3 client with assumed role
 
-session = boto3.Session(
+dynamodb = boto3.client(
+    "dynamodb",
     aws_access_key_id=creds["AccessKeyId"],
     aws_secret_access_key=creds["SecretAccessKey"],
     aws_session_token=creds["SessionToken"],
@@ -51,19 +54,33 @@ session = boto3.Session(
 # Attempt scanning "user" table, which is *not* allowed by policy
 
 try:
-    dynamodb = session.resource("dynamodb")
-    table = dynamodb.Table("user")
-    response = table.scan()
+    response = dynamodb.scan(TableName="users")
 except ClientError as e:
     print("Error: %s" % e)
 
-# Attempt GetItem on "pinehead_records_s3" table, which does *not* allow
-# the "price" attribute
+# Attempt query on "pinehead_records_s3" table, which does *not* allow
+# the "price" attribute. Adding "price" to the ProjectionExpression will
+# result in an error.
+
+session = boto3.Session(
+    aws_access_key_id=creds["AccessKeyId"],
+    aws_secret_access_key=creds["SecretAccessKey"],
+    aws_session_token=creds["SessionToken"],
+)
+
+dynamodb = session.resource("dynamodb")
 
 try:
-    dynamodb = session.resource("dynamodb")
+
     table = dynamodb.Table("pinehead_records_s3")
-    response = table.get_item(Key={"type": "album", "id": 15})
-    print(json.dumps(response["Item"], cls=DecimalEncoder, indent=4))
+
+    response = table.query(
+        KeyConditionExpression=Key("type").eq("album") & Key("id").eq(15),
+        ProjectionExpression="#T, id, name_title",
+        # ProjectionExpression="#T, id, name_title, price",  # AccessDeniedException
+        ExpressionAttributeNames={"#T": "type"},
+    )
+
+    print(json.dumps(response["Items"][0], cls=DecimalEncoder, indent=4))
 except ClientError as e:
     print("Error: %s" % e)
